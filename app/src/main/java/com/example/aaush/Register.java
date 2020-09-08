98857c95c7456dfc29167b293c690335968bc63c
karthika251199 <ponnada1999@gmail.com>
2020-09-08 10:25:27 +0530

    commit

--- app/src/main/java/com/example/aaush/Register.java
+++ app/src/main/java/com/example/aaush/Register.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -13,15 +15,24 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
     EditText mFullName,mEmail,mPassword,mConfirmPassword;
     Button mRegisterBtn;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +46,9 @@ public class Register extends AppCompatActivity {
         mConfirmPassword = findViewById(R.id.phone);
         mRegisterBtn = findViewById(R.id.registerBtn);
 
+
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         if(fAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(),Login.class));
@@ -45,8 +58,10 @@ public class Register extends AppCompatActivity {
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = mEmail.getText().toString().trim();
+                final String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+                final String fullName = mFullName.getText().toString();
+                final String phone = mConfirmPassword.getText().toString();
 
                 if (TextUtils.isEmpty(email)) {
                     mEmail.setError("Email is required");
@@ -67,9 +82,30 @@ public class Register extends AppCompatActivity {
 
                         if(task.isSuccessful()){
                             Toast.makeText(Register.this, "Successfully Registered!!", Toast.LENGTH_SHORT).show();
+
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("fName",fullName);
+                            user.put("email",email);
+                            user.put("phone",phone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("TAG","onSuccess:user profile created for"+userID);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d("TAG","onFailure:"+e.toString());
+
+                                }
+                            });
+
                             startActivity(new Intent(getApplicationContext(),Login.class));
                         }else{
                             Toast.makeText(Register.this, "Registration Unsuccessful", Toast.LENGTH_SHORT).show();
+
                         }
 
                     }
