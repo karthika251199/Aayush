62eb70b3ed60dbea0338d38b66e48c5d7dd85f14
karthika251199 <ponnada1999@gmail.com>
2020-09-10 15:51:18 +0530

    profile commit

--- app/src/main/java/com/example/aaush/HomePage.java
+++ app/src/main/java/com/example/aaush/HomePage.java
@@ -1,11 +1,15 @@
 package com.example.aaush;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -17,9 +21,44 @@ public class HomePage extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home_page);
     }
-    public void logout(View view){
+   /* public void logout(View view){
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(getApplicationContext(),Login.class));
-        finish();
+        finish();*/
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("profile");
+        menu.add("share");
+        menu.add("logout");
+
+
+        return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getTitle()=="profile") {
+            //Toast.makeText(this,"profile is Selected", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(getApplicationContext(),Profile.class));
+        }
+
+        else if(item.getTitle()=="share") {
+            Toast.makeText(this ,"share is Selected", Toast.LENGTH_SHORT).show();}
+
+        else if(item.getTitle()=="logout") {
+            //Toast.makeText( this,"logout is Selected", Toast.LENGTH_SHORT).show();
+               /* FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(),Login.class));
+                finish();*/
+            startActivity(new Intent(getApplicationContext(),Login.class));
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
+
+
---
+++ app/src/main/java/com/example/aaush/Profile.java
@@ -0,0 +1,48 @@
+package com.example.aaush;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+public class Profile extends AppCompatActivity {
+
+    TextView fullName,email,phone;
+    FirebaseFirestore fStore;
+    FirebaseAuth fAuth;
+    String userID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        phone = findViewById(R.id.profilePhone);
+        fullName = findViewById(R.id.profileName);
+        email = findViewById(R.id.profileEmail);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore =FirebaseFirestore.getInstance();
+
+        userID = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fStore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+
+                phone.setText(documentSnapshot.getString("phone"));
+                fullName.setText(documentSnapshot.getString("fName"));
+                email.setText(documentSnapshot.getString("email"));
+
+            }
+        });
+    }
+}
