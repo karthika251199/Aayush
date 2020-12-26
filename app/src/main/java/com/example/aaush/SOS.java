953cc632b9f2cb1b145f875d38b4cc4028bb2d30
PadminiPriyanka28 <prateekshaporeddy@gmail.com>
2020-12-26 14:55:45 +0530

    msg sos commit

--- app/src/main/java/com/example/aaush/SOS.java
+++ app/src/main/java/com/example/aaush/SOS.java
@@ -10,7 +10,9 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,10 +27,11 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class SOS extends AppCompatActivity {
     private static final int REQUEST_CALL=1;
-    private EditText mEditTextNumber;
+    private EditText mEditTextNumber,message;
     FirebaseFirestore fStore;
     FirebaseAuth fAuth;
     String userID;
+    private Button send;
 
 
     @Override
@@ -37,6 +40,8 @@ public class SOS extends AppCompatActivity {
         setContentView(R.layout.activity_s_o_s);
 
         mEditTextNumber = findViewById(R.id.phone);
+        message = findViewById(R.id.msg);
+        send = findViewById(R.id.bt_msg);
         Button bt_call = findViewById(R.id.bt_call);
 
         fAuth = FirebaseAuth.getInstance();
@@ -58,6 +63,20 @@ public class SOS extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 makePhoneCall();
+            }
+        });
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                    if (checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                        sendSMS();
+                    }else {
+                        requestPermissions(new String[] {Manifest.permission.SEND_SMS},1);
+                    }
+                }
             }
         });
 
@@ -91,5 +110,22 @@ public class SOS extends AppCompatActivity {
                 Toast.makeText(this, "permission DENIED", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    private void sendSMS () {
+        String phoneNo = mEditTextNumber.getText().toString().trim();
+        String sms = message.getText().toString().trim();
+
+        try {
+            SmsManager smsManager=SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo,null, sms,null,null);
+            Toast.makeText(this, "Message is sent",Toast.LENGTH_SHORT).show();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            Toast.makeText(this, "Faied to send msg",Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
